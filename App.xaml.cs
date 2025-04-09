8217e51334ad8e2159bf07a53ea37683c64a8dc3
Juan Manuel Benitez <jm_benitez@outlook.com>
2025-04-09 07:14:27 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MD_Scheduler_Software_II
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
